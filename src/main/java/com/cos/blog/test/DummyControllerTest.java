f5f9e0b019d6d23991b54883d3aac187e14e9793
HongDongHwan <hongdh94@gmail.com>
2023-02-03 16:13:53 +0900

    0203 ADD ajax통신을 활용하여 회원가입 기능구현

--- src/main/java/com/cos/blog/test/DummyControllerTest.java
+++ src/main/java/com/cos/blog/test/DummyControllerTest.java
@@ -1,7 +1,22 @@
 package com.cos.blog.test;
 
+import java.util.List;
+import java.util.function.Supplier;
+
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cos.blog.model.RoleType;
@@ -13,6 +28,77 @@ public class DummyControllerTest {
 
 	@Autowired // 의존성 주입
 	private UserRepository userRepository;
+	
+	
+	// delete
+	@DeleteMapping("/dummy/user/{id}")
+	public String delete(@PathVariable int id) {
+		try {
+			userRepository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			return "삭제실패 해당아이디 없음";
+		}
+		
+		return "삭제 되었습니다~ id : " + id;
+	}
+	
+	// email, password
+	@Transactional // 함수 종료시에 자동 commit이 됨.
+	@PutMapping("/dummy/user/{id}")
+	public User updateUser(@PathVariable int id, @RequestBody User requestUser) {
+		System.out.println("id : " + id);
+		System.out.println("password : " + requestUser.getPassword());
+		System.out.println("email : " + requestUser.getEmail());
+		
+		User user = userRepository.findById(id).orElseThrow(() -> {
+			return new IllegalArgumentException("수정에 실패했");
+		});
+		
+		user.setPassword(requestUser.getPassword());
+		user.setEmail(requestUser.getEmail());
+		
+		// userRepository.save(user);
+		
+		// 더티 체킹
+		return user;
+	}
+	
+	
+	@GetMapping("/dummy/users")
+	public List<User> list() {
+		return userRepository.findAll();
+	}
+	
+	// 한 페이지당 2건의 데이터를 리턴받아 볼 예정
+	@GetMapping("/dummy/user")
+	public List<User> pageList(@PageableDefault(size = 2, sort = "id", direction = Direction.DESC) Pageable pageable) {
+		Page<User> pagingUser = userRepository.findAll(pageable);
+		List<User> users = pagingUser.getContent();
+		return users;
+	}
+	
+	// {id} 주소로 파라미터를 전달 받을 수 있음.
+	@GetMapping("/dummy/user/{id}")
+	public User detail(@PathVariable int id) {
+		
+		User user = userRepository.findById(id).orElseThrow(new Supplier<IllegalArgumentException>() {
+
+			@Override
+			public IllegalArgumentException get() {
+				// TODO Auto-generated method stub
+				return new IllegalArgumentException("해당 유저는 없습니다~ id : " + id);
+			}
+		
+		});
+		// 요청 : 웹브라우저 
+		// user 객체 = 자바 오브젝트
+		// 변환(웹브라우저가 이해할 수 있는 데이터) -> json
+		// 스프링 부트 = MessageConverter라는 애가 응답시에 자동 작동!
+		// 만약에 자바 오브젝트를 리턴하게 되면 MessageConverter가 Jackson라이브러리를 호출해서
+		// user오브젝트를 json으로 변환해서 브라우저에게 던져준다.
+		return user;
+	}
+	
 	
 	@PostMapping("/dummy/join")
 	public String join(User user) { // key=value(약속된규칙)
